5bb09c3c8163f441bcc8c846ed5915bf3e4f0051
Htet Yamin Ei 47743220 <htetyamin.ei@students.mq.edu.au>
2025-04-02 17:10:50 +1100

    Set a color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 65, 86, 149),
+);
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses( ),
-    )
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          )),
+      home: const Expenses(),
+    ),
   );
 }
